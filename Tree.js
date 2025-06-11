b94e7f4508f40b454689a1c748f278d0ecaf0ec2
hencci <hencci20@gmail.com>
2025-06-11 14:18:56 +0100

    Create a Tree class which has a root attribute that uses a buildTree function value

--- Tree.js
+++ Tree.js
@@ -4,4 +4,17 @@ export class Tree {
     constructor(array) {
         this.root = this.buildTree([...new Set(array)].sort((a, b) => a - b));
     }
+
+    // Recursively builds a balanced BST
+    buildTree(array) {
+        if (array.length === 0) return null;
+
+        const mid = Math.floor(array.length / 2);
+        const root = new Node(array[mid]);
+
+        root.left = this.buildTree(array.slice(0, mid));
+        root.right = this.buildTree(array.slice(mid + 1));
+
+        return root;
+    }
 }
