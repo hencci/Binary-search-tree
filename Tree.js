87456e58108de833fb3d6ca78152ce382deb8e02
hencci <hencci20@gmail.com>
2025-06-11 17:09:48 +0100

    Write insert(value) and deleteItem(value) functions that insert/delete the given value

--- Tree.js
+++ Tree.js
@@ -17,4 +17,39 @@ export class Tree {
 
         return root;
     }
+    
+    insert(value, node = this.root) {
+        if (!node) return new Node(value);
+
+        if (value === node.data) return node;
+
+        if (value < node.data) {
+            node.left = this.insert(value, node.left);
+        }
+        else {
+            node.right = this.insert(value, node.right);
+        }
+
+        return node;
+    }
+
+    deleteItem(value, node = this.root) {
+        if (!node) return null;
+
+        if (value < node.data) {
+        node.left = this.deleteItem(value, node.left);
+        } else if (value > node.data) {
+        node.right = this.deleteItem(value, node.right);
+        } else {
+        if (!node.left && !node.right) return null;
+        if (!node.left) return node.right;
+        if (!node.right) return node.left;
+
+        const successor = this.minValueNode(node.right);
+        node.data = successor.data;
+        node.right = this.deleteItem(successor.data, node.right);
+        }
+
+        return node;
+    }
 }
